1933bd70da256b0ff5b38d2c3574234f6728e35f
Marshal <>
2022-10-21 17:29:39 +0800

    no message

--- lib/permission.dart
+++ lib/permission.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter_progress_hud/flutter_progress_hud.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
@@ -33,11 +36,12 @@ class PermissionTestWidget extends StatelessWidget {
                       //提前请求权限，如果没给过权限，可以触发权限，以便于获取权限信息
                       final status = await Permission.camera.request();
                       //可以同时请求多个 await
+                      if (status != PermissionStatus.granted) {
+                        //无相机权限，请前往设置打开
+                        //openAppSettings();
+                        return;
+                      }
                       print(status);
-                      //直接
-                      final statuss = await Permission.camera.status;
-                      print(statuss);
-                      return;
                       _picker.pickImage(source: ImageSource.camera).then((value) {
                         print(value);
                       }).catchError((error) {
@@ -45,7 +49,7 @@ class PermissionTestWidget extends StatelessWidget {
                         //可以请求用失败的时候在提示权限问题，或者打开授权页面
                         Permission.camera.status.then((status) {
                           print(status);
-                          if (status == PermissionStatus.denied) {
+                          if (status == PermissionStatus.denied || status == PermissionStatus.permanentlyDenied) {
                             //拒绝，可以跳转权限页面
                             showToast(context, "相机权限被拒绝");
                           }else if (status == PermissionStatus.granted) {
@@ -58,14 +62,15 @@ class PermissionTestWidget extends StatelessWidget {
                     child: const Text("测试相机权限"),
                   ),
                   MaterialButton(
-                    onPressed: () async {
+                    onPressed: () {
+                      //也可以先访问，失败后在查看权限，不是所有库都能这么做，最好先判断
                       _picker.pickImage(source: ImageSource.gallery).then((value) {
                         print(value);
                       }).catchError((error) {
                         //注意相册使用的是这个权限，而不是媒体库
                         Permission.photos.status.then((status) {
                           print(status);
-                          if (status == PermissionStatus.denied) {
+                          if (status == PermissionStatus.denied || status == PermissionStatus.permanentlyDenied) {
                             //拒绝，可以跳转权限页面
                             showToast(context, "相册权限被拒绝");
                           }else if (status == PermissionStatus.granted) {
@@ -75,42 +80,42 @@ class PermissionTestWidget extends StatelessWidget {
                         });
                       });
                     },
-                    child: const Text("测试媒体库相册权限"),
+                    child: const Text("测试写入图片权限"),
                   ),
                   MaterialButton(
                     onPressed: () async {
-                      //查看相册，如果只是添加，可以采用 photosAddOnly
+                      //查看相册，如果只是添加，可以采用 photosAddOnly(这里测试同时请求多个的方法)
                       Map<Permission, PermissionStatus> statuses = await [
-                        Permission.mediaLibrary,
+                        Permission.photosAddOnly,
                       ].request();
                       print(statuses);
-
-                      Permission.mediaLibrary.status.then((status) {
+                      if (statuses[Permission.photosAddOnly] != PermissionStatus.granted) {
+                        //写入相册权限被拒绝
+                        print("写入相册权限被拒绝");
+                        return;
+                      }
+                      // final result = await ImageGallerySaver.saveImage(
+                      //     Uint8List.fromList(data), //data字节数组
+                      //     quality: 60, //压缩质量 0~100
+                      //     name: "hello"//名字
+                      // );
+                      Permission.photosAddOnly.status.then((status) {
                         print(status);
-                        if (status == PermissionStatus.denied) {
+                        if (status == PermissionStatus.denied || status == PermissionStatus.permanentlyDenied) {
                           //拒绝，可以跳转权限页面
-                          showToast(context, "媒体库权限被拒绝");
+                          showToast(context, "写入相册权限被拒绝");
                         }else if (status == PermissionStatus.granted) {
                           //正常访问
-                          showToast(context, "媒体库权限可以正常使用");
+                          showToast(context, "可以正常使用写入相册");
                         }
                       });
-                      //查看相册，如果只是添加，可以采用 photosAddOnly
-                      // Permission.photos.status.then((status) {
-                      //   print(status);
-                      //   if (status == PermissionStatus.denied) {
-                      //     //拒绝
-                      //     showToast(context, "相册权限被拒绝");
-                      //   }else if (status == PermissionStatus.granted) {
-                      //     //正常访问
-                      //     showToast(context, "相机权限可以正常使用");
-                      //   }
-                      // });
                     },
-                    child: const Text("测试图片资源权限(和媒体库不是一个，这个访问全部,可以写入照片)"),
+                    child: const Text("写入相册权限"),
                   ),
                   MaterialButton(
-                    onPressed: () {
+                    onPressed: () async {
+                      await Permission.locationWhenInUse.request();
+
                       Permission.locationWhenInUse.status.then((status) {
                         print(status);
                         if (status == PermissionStatus.denied) {
@@ -121,7 +126,6 @@ class PermissionTestWidget extends StatelessWidget {
                           showToast(context, "定位权限可以正常使用");
                         }
                       });
-
                     },
                     child: const Text("测试定位权限"),
                   ),
@@ -129,7 +133,7 @@ class PermissionTestWidget extends StatelessWidget {
                     onPressed: () {
                       Permission.bluetooth.status.then((status) {
                         print(status);
-                        if (status == PermissionStatus.denied) {
+                        if (status == PermissionStatus.denied || status == PermissionStatus.permanentlyDenied) {
                           //拒绝
                           showToast(context, "蓝牙权限被拒绝");
                         }else if (status == PermissionStatus.granted) {
