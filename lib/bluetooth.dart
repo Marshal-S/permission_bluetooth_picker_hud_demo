a9dfc25e02776e379d323badaec9fa69de8e4bbe
Marshal <>
2022-11-01 15:17:53 +0800

    no message

--- lib/bluetooth.dart
+++ lib/bluetooth.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 const String serviceUUID = "11000000-0000-0000-0000-818282828282";
 const String characterUUID = "12000000-0000-0000-0000-000000000000";
@@ -21,6 +23,21 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
   BluetoothCharacteristic? _currentCharacteristic;
 
   String reviceText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    //请求一下权限，某些机型蓝牙默认可以直接使用，无需权限，但是权限会反馈被拒绝状态
+    //因此一些机型，可以请求后给出继续向后执行的弹窗，避免用户永远无法使用该功能，失败了注意反馈即可
+    [Permission.bluetoothScan, Permission.bluetoothConnect, Permission.bluetooth].request().then((status) {
+      print(status);
+      //只会有一个弹窗，android端多个权限，但一般一般显示了一个就结束了，主要看ios的
+      //android端注意定位权限, Permission.locationWhenInUse 即可
+      if (status[Permission.bluetooth] != PermissionStatus.granted) {
+        print("没有蓝牙权限"); //这一个是都有的，可以用这个判断，当然最好分平台判断
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -100,7 +117,7 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
   }
 
   //开始扫描并连接设备
-  Future<void> startConnectDevice({isRepeat = false}) async {
+  void startConnectDevice({isRepeat = false}) {
     bool isSearched = false; //为了避免连接过程中没有停止扫描导致的多次连接问题，但不得不连接后在取消扫描
     try {
       _bluetooth.scanResults.listen((results) async {
@@ -116,6 +133,7 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
             isSearched = true; //标记已经找到了
             //找到了我们要连接的设备,并连接,autoConnect需要设置为 false，默认为true其不会自动连接
             print("开始连接");
+            //鸿蒙这个 autoConnect 会一次也连接不上，设置为 false 则会出现时而连得上时而连不上，支持不太好，根据情况选择
             await device.connect(timeout: const Duration(seconds: 10), autoConnect: true);
             print("连接成功");
             // 连接成功后停止扫描，据说有些android手机停止扫描会出现连接功能异常，连接成功后在停止扫描
@@ -126,7 +144,7 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
             //遍历服务使用我们想要的特征值
             print("遍历服务找特征值");
             print(services.length);
-            services.forEach((service) {
+            services.forEach((service) async {
               var characteristics = service.characteristics;
               //找我们的特征值
               for(BluetoothCharacteristic char in characteristics) {
@@ -136,8 +154,8 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
                   print("找到我们特征值");
                   //假设我们用这个,获取到特征值后保存，可以用来读写
                   _currentCharacteristic = char;
-                  setNotifiy();//设置通知，可以接收传递过来的消息
-                  readMessage(); //随便读取一下消息吧
+                  await setNotifiy();//设置通知，可以接收传递过来的消息
+                  await readMessage(); //随便读取一下消息吧
                 }
               }
             });
@@ -147,17 +165,17 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
     }catch(error) {
       print(error);
       isSearched = false;
-      print('在尝试一次');
-      await _bluetooth.stopScan();
+      print('一般连接失败后会走这里，然后停止扫描');
+      _bluetooth.stopScan();
     }
 
     print("开始扫描");
     _bluetooth.startScan(scanMode: ScanMode.balanced, timeout: const Duration(seconds: 30),);
   }
 
-  void setNotifiy() {
+  Future<void> setNotifiy() async {
     //可以监听外设端主动发送过来的消息
-    _currentCharacteristic!.setNotifyValue(true);
+    await _currentCharacteristic!.setNotifyValue(true);
     _currentCharacteristic!.value.listen((event) {
       print(event);
     });
@@ -168,6 +186,8 @@ class _BluetoothTestWidgetState extends State<BluetoothTestWidget> {
     if (_currentCharacteristic == null) return;
 
     List<int> value = await _currentCharacteristic!.read();
+    print("value");
+    print(value);
     final string = utf8.decode(value);
     setState(() {
       reviceText = string;
